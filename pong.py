e1f215ae883812c77bc7617a63bc54b277b920ab
Alpha_Lin <alpha_lin@protonmail.com>
2020-11-14 14:29:48 +0100

    - ajout de commentaires - ajout de plusieurs briques

--- pong.py
+++ pong.py
@@ -1,18 +1,21 @@
 import random # Pour les tirages
-import sys
-import pygame
-import pygame.freetype
+import sys # Pour quitter proprement
+import pygame # Le module Pygame
+import pygame.freetype # Pour afficher du texte
 import math
 
-pygame.init()
+pygame.init() # initialisation de Pygame
 
+# Pour le texte.
 pygame.freetype.init()
-myfont=pygame.freetype.SysFont(None, 20)
+myfont=pygame.freetype.SysFont(None, 20) # texte de taille 20
 
+# Taille de la fenetre
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Ping")
 
+# Pour limiter le nombre d'images par seconde
 clock=pygame.time.Clock()
 
 BLANC = (255, 255, 255)
@@ -29,8 +32,8 @@ class Balle:
 
     def __init__(self):
         self.x, self.y = (400, 400)
-        self.vitesse = 8
-        self.vitesse_par_angle(60)
+        self.vitesse = 8 # vitesse initiale
+        self.vitesse_par_angle(60) # vecteur vitesse
         self.sur_raquette = True
 
     def afficher(self):
@@ -64,13 +67,14 @@ class Jeu:
     def __init__(self):
         self.balle = Balle()
         self.raquette = Raquette()
-        self.brique = Brique(400, 100)
+        self.lignesBriques = [[Brique(i * 70 + 25 + 15, j * 50 + 15 + 15) for j in range(5)] for i in range(11)]
 
     def gestion_evenements(self):
+        # Gestion des evenements
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.QUIT: sys.exit() # Pour quitter
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
+                if event.button == 1: # Bouton gauche
                     if self.balle.sur_raquette:
                         self.balle.sur_raquette = False
                         self.balle.vitesse_par_angle(60)
@@ -78,16 +82,20 @@ class Jeu:
     def mise_a_jour(self):
         x, y = pygame.mouse.get_pos()
         self.balle.deplacer(self.raquette)
-        if self.brique.en_vie():
-            self.brique.collision_balle(self.balle)
+        for briques in self.lignesBriques:
+            for brique in briques:
+                if brique.en_vie():
+                    brique.collision_balle(self.balle)
         self.raquette.deplacer(x)
 
     def affichage(self):
-        screen.fill(NOIR)
+        screen.fill(NOIR) # on efface l'écran
         self.balle.afficher()
         self.raquette.afficher()
-        if self.brique.en_vie():
-            self.brique.afficher()
+        for briques in self.lignesBriques:
+            for brique in briques:
+                if brique.en_vie():
+                    brique.afficher()
 
 class Raquette:
     def __init__(self):
@@ -124,28 +132,29 @@ class Brique:
 
     def afficher(self):
         pygame.draw.rect(screen, BLANC, (int(self.x-self.longueur/2),
-                                        int(self.y-self.longueur/2),
+                                        int(self.y-self.largeur/2),
                                         self.longueur, self.largeur), 0)
 
     def collision_balle(self, balle):
+        # on suppose que largeur<longueur
         marge = self.largeur/2 + RAYON_BALLE
         dy = balle.y - self.y
         touche = False
-        if balle.x >= self.x:
+        if balle.x >= self.x: # on regarde a droite
             dx = balle.x - (self.x + self.longueur/2 - self.largeur/2)
-            if abs(dy) <= marge and dx <= marge:
+            if abs(dy) <= marge and dx <= marge: # on touche
                 touche = True
                 if dx <= abs(dy):
                     balle.vy = -balle.vy
-                else:
+                else: # a droite
                     balle.vx = -balle.vx
-        else:
+        else: # on regarde a gauche
             dx = balle.x - (self.x - self.longueur/2 + self.largeur/2)
-            if abs(dy) <= marge and -dx <= marge:
+            if abs(dy) <= marge and -dx <= marge: # on touche
                 touche = True
                 if -dx <= abs(dy):
                     balle.vy = -balle.vy
-                else:
+                else: # a gauche
                     balle.vx = -balle.vx
         if touche:
             self.vie -= 1
@@ -158,5 +167,5 @@ while True:
     jeu.mise_a_jour()
     jeu.affichage()
 
-    pygame.display.flip()
-    clock.tick(60)
+    pygame.display.flip() # envoie de l'image à la carte graphique
+    clock.tick(60) # on attend pour ne pas dépasser 60 images/seconde
